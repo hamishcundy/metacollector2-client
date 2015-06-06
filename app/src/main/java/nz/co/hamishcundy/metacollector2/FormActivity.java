19a30b4d0309302810dc83a674ba2bc3f6ee2767
Hamish Cundy <hamishcundy@gmail.com>
2015-06-07 10:21:15 +1200

    Wizardpager setup

--- app/src/main/java/nz/co/hamishcundy/metacollector2/FormActivity.java
+++ app/src/main/java/nz/co/hamishcundy/metacollector2/FormActivity.java
@@ -2,23 +2,25 @@ package nz.co.hamishcundy.metacollector2;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class FormActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_form);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.action_bar);
+        setSupportActionBar(toolbar);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_form, menu);
         return true;
     }
 
