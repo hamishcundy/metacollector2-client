a2daa71cccd127a437636f8db26b2f8322ba2bc1
Hamish Cundy <hamishcundy@gmail.com>
2015-06-07 19:25:17 +1200

    Terms and conditions page

---
+++ app/src/main/java/nz/co/hamishcundy/metacollector2/ui/TermsAndConditionsFragment.java
@@ -0,0 +1,92 @@
+package nz.co.hamishcundy.metacollector2.ui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.TextView;
+
+
+import com.tech.freak.wizardpager.ui.PageFragmentCallbacks;
+
+import nz.co.hamishcundy.metacollector2.R;
+
+/**
+ * Created by TechFreak on 04/09/2014.
+ */
+public class TermsAndConditionsFragment extends Fragment {
+    private static final String ARG_KEY = "key";
+
+    private PageFragmentCallbacks mCallbacks;
+    private String mKey;
+    private TermsAndConditionsPage mPage;
+
+
+    public static TermsAndConditionsFragment create(String key) {
+        Bundle args = new Bundle();
+        args.putString(ARG_KEY, key);
+
+        TermsAndConditionsFragment fragment = new TermsAndConditionsFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public TermsAndConditionsFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle args = getArguments();
+        mKey = args.getString(ARG_KEY);
+        mPage = (TermsAndConditionsPage) mCallbacks.onGetPage(mKey);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_terms_and_conditions, container, false);
+        ((TextView) rootView.findViewById(android.R.id.title)).setText(mPage.getTitle());
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+
+        if (!(activity instanceof PageFragmentCallbacks)) {
+            throw new ClassCastException("Activity must implement PageFragmentCallbacks");
+        }
+
+        mCallbacks = (PageFragmentCallbacks) activity;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mCallbacks = null;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+    @Override
+    public void setMenuVisibility(boolean menuVisible) {
+        super.setMenuVisibility(menuVisible);
+
+
+    }
+}
--- app/src/main/java/nz/co/hamishcundy/metacollector2/ui/UserFlowModel.java
+++ app/src/main/java/nz/co/hamishcundy/metacollector2/ui/UserFlowModel.java
@@ -4,8 +4,11 @@ import android.content.Context;
 
 import com.tech.freak.wizardpager.model.AbstractWizardModel;
 import com.tech.freak.wizardpager.model.BranchPage;
+import com.tech.freak.wizardpager.model.MultipleFixedChoicePage;
+import com.tech.freak.wizardpager.model.NumberPage;
 import com.tech.freak.wizardpager.model.PageList;
 import com.tech.freak.wizardpager.model.SingleFixedChoicePage;
+import com.tech.freak.wizardpager.model.TextPage;
 
 /**
  * Created by hamish on 7/06/15.
@@ -20,6 +23,47 @@ public class UserFlowModel extends AbstractWizardModel {
 
     @Override
     protected PageList onNewRootPageList() {
-        return new PageList(new BranchPage(this, "Test branch").addBranch("Branch choice 1", new SingleFixedChoicePage(this, "Choice page 1").setChoices("1", "2")));
+        //return new PageList(new BranchPage(this, "Test branch").addBranch("Branch choice 1", new SingleFixedChoicePage(this, "Choice page 1").setChoices("1", "2")).addBranch("Branch choice 2", new SingleFixedChoicePage(this, "Choice page 2").setChoices("3", "4")));
+        return new PageList(new BranchPage(this, "Order type")
+                .addBranch(
+                        "Sandwich",
+                        new SingleFixedChoicePage(this, "Bread").setChoices("White",
+                                "Wheat", "Rye", "Pretzel", "Ciabatta")
+                                .setRequired(true),
+
+                        new MultipleFixedChoicePage(this, "Meats").setChoices(
+                                "Pepperoni", "Turkey", "Ham", "Pastrami", "Roast Beef",
+                                "Bologna"),
+
+                        new MultipleFixedChoicePage(this, "Veggies").setChoices(
+                                "Tomatoes", "Lettuce", "Onions", "Pickles",
+                                "Cucumbers", "Peppers"),
+
+                        new MultipleFixedChoicePage(this, "Cheeses").setChoices(
+                                "Swiss", "American", "Pepperjack", "Muenster",
+                                "Provolone", "White American", "Cheddar", "Bleu"),
+
+                        new BranchPage(this, "Toasted?")
+                                .addBranch(
+                                        "Yes",
+                                        new SingleFixedChoicePage(this, "Toast time")
+                                                .setChoices("30 seconds", "1 minute",
+                                                        "2 minutes")).addBranch("No")
+                                .setValue("No"))
+
+                .addBranch(
+                        "Salad",
+                        new SingleFixedChoicePage(this, "Salad type").setChoices(
+                                "Greek", "Caesar").setRequired(true),
+
+                        new SingleFixedChoicePage(this, "Dressing").setChoices(
+                                "No dressing", "Balsamic", "Oil & vinegar",
+                                "Thousand Island", "Italian").setValue("No dressing"),
+                        new NumberPage(this, "How Many Salads?").setRequired(true)),
+                new TextPage(this, "Text").setRequired(true)
+
+                        .setRequired(true),
+
+                new TermsAndConditionsPage(this, "Terms and Conditions").setRequired(true));
     }
 }
