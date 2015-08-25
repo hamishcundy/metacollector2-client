a944ab8bfefdb256a2f33f0741b83e2631eb8a07
Hamish Cundy <hamishcundy@gmail.com>
2015-08-25 19:02:06 +1200

    Facebook api setup

--- app/src/main/java/nz/co/hamishcundy/metacollector2/collection/FacebookSource.java
+++ app/src/main/java/nz/co/hamishcundy/metacollector2/collection/FacebookSource.java
@@ -1,9 +1,18 @@
 package nz.co.hamishcundy.metacollector2.collection;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.facebook.AccessToken;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
+import nz.co.hamishcundy.metacollector2.data.records.FacebookRootRecord;
 import nz.co.hamishcundy.metacollector2.data.records.MetadataRecord;
 
 /**
@@ -16,7 +25,20 @@ public class FacebookSource extends MetadataCollectionSource {
 
     @Override
     public List<MetadataRecord> retrieveRecords(Context con) {
-        return null;
+        GraphRequest request = GraphRequest.newMeRequest(AccessToken.getCurrentAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+
+            @Override
+            public void onCompleted(JSONObject jsonObject, GraphResponse graphResponse) {
+
+            }
+        });
+        GraphResponse respo = request.executeAndWait();
+        Log.d("FS", respo.getJSONObject().toString());
+        List<MetadataRecord> list = new ArrayList<MetadataRecord>();
+        FacebookRootRecord frr = new FacebookRootRecord();
+        list.add(frr);
+        return list;
+
     }
 
     @Override
---
+++ app/src/main/java/nz/co/hamishcundy/metacollector2/data/records/FacebookRootRecord.java
@@ -0,0 +1,7 @@
+package nz.co.hamishcundy.metacollector2.data.records;
+
+/**
+ * Created by hamish on 25/08/15.
+ */
+public class FacebookRootRecord implements MetadataRecord{
+}
